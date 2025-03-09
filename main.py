54e5788ae263ebd1c4301fa0f17c760870a290bd
anangyoga <aharwinto@gmail.com>
2025-03-09 23:51:25 +0700

    fix: make it work

--- main.py
+++ main.py
@@ -6,22 +6,14 @@ st.set_page_config(page_title="UTM Generator", page_icon="📝")
 st.title("UTM URL Builder")
 st.write("This tool allows you to easily add campaign parameters to URLs so you can measure in :blue[Google Analytics].")
 
-if "utm_url" not in st.session_state:
-    st.session_state.utm_url = ""
-    st.session_state.utm_id = ""
-    st.session_state.utm_src = ""
-    st.session_state.utm_medium = ""
-    st.session_state.utm_name = ""
-    st.session_state.utm_term = ""
-    st.session_state.utm_content = ""
 
-utm_url = st.text_input("Website URL* *(required)*", st.session_state.utm_url)
-utm_id = st.text_input("Campaign ID", st.session_state.utm_id)
-utm_src = st.text_input("Campaign Source* *(required)*", st.session_state.utm_src)
-utm_medium = st.text_input("Campaign Medium* *(required)*", st.session_state.utm_medium)
-utm_name = st.text_input("Campaign Name", st.session_state.utm_name)
-utm_term = st.text_input("Campaign Term", st.session_state.utm_term)
-utm_content = st.text_input("Campaign Content", st.session_state.utm_content)
+utm_url = st.text_input("Website URL* *(required)*")
+utm_id = st.text_input("Campaign ID")
+utm_src = st.text_input("Campaign Source* *(required)*")
+utm_medium = st.text_input("Campaign Medium* *(required)*")
+utm_name = st.text_input("Campaign Name* *(required)*")
+utm_term = st.text_input("Campaign Term")
+utm_content = st.text_input("Campaign Content")
 
 generate_utm = st.button("Generate UTM")
 
@@ -42,16 +34,5 @@ if generate_utm:
         final_url = f"{utm_url}?{utm_query}"
         st.write(final_url)
 
-        st.session_state.utm_url = ""
-        st.session_state.utm_id = ""
-        st.session_state.utm_src = ""
-        st.session_state.utm_medium = ""
-        st.session_state.utm_name = ""
-        st.session_state.utm_term = ""
-        st.session_state.utm_content = ""
-
-        st.rerun()
-    else:
-        st.error("Please enter a valid Website URL and required UTM parameters.")
 
     add_to_csv = st.button("Add to Database")
